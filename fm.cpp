95b39cf36151d0123902f0fc0fe8ffb01612ff05
Mandzheev <mandzheevdmitriy@gmail.com>
2021-05-03 02:48:13 +0500

    add class League

--- fm.cpp
+++ fm.cpp
@@ -4,13 +4,14 @@
 
 using namespace std;
 
+int num;
+string line[20];
+
 class Leagues {
 public:
   Leagues() {
     int r = 1;
-    int num;
     int g = 1;
-    string line[20];
     ifstream in("list_leagues.txt");
 
     if (in.is_open()) {
@@ -23,29 +24,37 @@ public:
       for (int i = 1; i < 20; i++)
         getline(in, line[i]);
       cin >> num;
-      cout << line[num] << endl;
     }
     in.close();
   }
 };
 
-/*class League {
-  name list_team
+class League {
+public:
+  string tm;
+  League() {
+
+    char tx[] = ".txt";
+
+    tm = line[num] + tx;
+
+    cout << tm << endl;
+  } // ifstream in(tm);
 };
 
-class Team {
-  name couch list_player
-};
-
-//class player {
-    //number  name
-      //  amplua
- //       skill
+// class Team {
+// name couch list_player
 //};
-*/
+
+// class player {
+// number  name
+//  amplua
+//       skill
+//};
+
 int main() {
 
   Leagues l1;
-
+  League l2;
   return 0;
 }
